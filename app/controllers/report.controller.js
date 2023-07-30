92fa937f1d6ea4f801cda44ed9cb4d2dae094ffa
Naeem Ahmad <61089439+knavekaiser@users.noreply.github.com>
2023-07-30 10:13:35 +0600

    analytics updates

--- app/controllers/report.controller.js
+++ app/controllers/report.controller.js
@@ -6,15 +6,22 @@ const { Chat } = require("../models");
 
 exports.getAnalytics = async (req, res) => {
   try {
+    const { granularity, minDate, maxDate } = req.query;
     const condition = {};
     if (req.business) {
-      condition.user = req.business._id;
+      condition.business = req.business._id;
     }
-    if (req.authToken.user_type === "business") {
-      condition.user = req.authUser._id;
+    if (req.authToken.userType === "business") {
+      condition.business = req.authUser._id;
     }
-    const fromDate = new Date().deduct("0 0 0 28");
-    const toDate = new Date();
+    const fromDate = minDate
+      ? new Date(minDate)
+      : new Date().deduct("0 0 0 28");
+    const toDate = maxDate ? new Date(maxDate) : new Date();
+    condition.createdAt = {
+      $gte: new Date(fromDate),
+      $lte: new Date(toDate),
+    };
     Promise.all([
       Chat.aggregate([
         {
@@ -22,47 +29,47 @@ exports.getAnalytics = async (req, res) => {
             createdAt: -1,
           },
         },
-        {
-          $match: {
-            createdAt: {
-              $gte: fromDate,
-              $lte: toDate,
-            },
-          },
-        },
+        { $match: condition },
         {
           $addFields: {
             date: {
-              $concat: [
-                {
-                  $toString: {
-                    $dayOfMonth: "$createdAt",
-                  },
-                },
-                "-",
-                {
-                  $toString: {
-                    $month: "$createdAt",
-                  },
-                },
-                "-",
-                {
-                  $toString: {
-                    $year: "$createdAt",
-                  },
-                },
-              ],
+              $concat:
+                granularity === "month"
+                  ? [
+                      {
+                        $toString: {
+                          $month: "$createdAt",
+                        },
+                      },
+                      "-",
+                      {
+                        $toString: {
+                          $year: "$createdAt",
+                        },
+                      },
+                    ]
+                  : [
+                      {
+                        $toString: {
+                          $dayOfMonth: "$createdAt",
+                        },
+                      },
+                      "-",
+                      {
+                        $toString: {
+                          $month: "$createdAt",
+                        },
+                      },
+                      "-",
+                      {
+                        $toString: {
+                          $year: "$createdAt",
+                        },
+                      },
+                    ],
             },
           },
         },
-        // {
-        //   $group: {
-        //     _id: "$date",
-        //     totalChats: {
-        //       $sum: 1,
-        //     },
-        //   },
-        // },
         {
           $facet: {
             chatsOverTime: [
@@ -85,27 +92,73 @@ exports.getAnalytics = async (req, res) => {
                 },
               },
             ],
+            tokenUsageOverTime: [
+              {
+                $group: {
+                  _id: "$date",
+                  totalUsage: {
+                    $sum: {
+                      $reduce: {
+                        input: "$messages",
+                        initialValue: 0,
+                        in: {
+                          $add: [
+                            "$$value",
+                            {
+                              $cond: [
+                                {
+                                  $and: [
+                                    {
+                                      $ifNull: ["$$this.token", false],
+                                    },
+                                    {
+                                      $ne: ["$$this.token", null],
+                                    },
+                                  ],
+                                },
+                                "$$this.token",
+                                0,
+                              ],
+                            },
+                          ],
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            ],
           },
         },
       ]),
     ])
-      .then(([[{ chatsOverTime, chatsByTopic }]]) => {
+      .then(([[{ chatsOverTime, chatsByTopic, tokenUsageOverTime }]]) => {
         const labels = [];
         while (fromDate <= toDate) {
-          labels.push(
-            `${new Date(fromDate).getDate()}-${
-              new Date(fromDate).getMonth() + 1
-            }-${new Date(fromDate).getFullYear()}`
-          );
-          fromDate.setDate(fromDate.getDate() + 1);
+          if (granularity === "month") {
+            labels.push(
+              `${new Date(fromDate).getMonth() + 1}-${new Date(
+                fromDate
+              ).getFullYear()}`
+            );
+            fromDate.setMonth(fromDate.getMonth() + 1);
+          } else {
+            labels.push(
+              `${new Date(fromDate).getDate()}-${
+                new Date(fromDate).getMonth() + 1
+              }-${new Date(fromDate).getFullYear()}`
+            );
+            fromDate.setDate(fromDate.getDate() + 1);
+          }
         }
         chatsOverTime = chatsOverTime.reduce((p, c) => {
           p[c._id] = c.totalChats;
           return p;
         }, {});
-
-        const topicLabels = chatsByTopic.map((item) => item._id);
-        chatsByTopic = chatsByTopic.map((item) => item.totalChats);
+        tokenUsageOverTime = tokenUsageOverTime.reduce((p, c) => {
+          p[c._id] = c.totalUsage;
+          return p;
+        }, {});
 
         responseFn.success(res, {
           data: {
@@ -114,8 +167,12 @@ exports.getAnalytics = async (req, res) => {
               data: labels.map((date) => chatsOverTime[date] || 0),
             },
             chatsByTopic: {
-              labels: topicLabels,
-              data: chatsByTopic,
+              labels: chatsByTopic.map((item) => item._id),
+              data: chatsByTopic.map((item) => item.totalChats),
+            },
+            tokenUsageOverTime: {
+              labels,
+              data: labels.map((date) => tokenUsageOverTime[date] || 0),
             },
           },
         });
