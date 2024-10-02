a73c09e3f755fcecd769cd9a7895f33ce7681c76
Naeem Ahmad <naeem.ahmad.9m@gmail.com>
2024-10-02 20:00:35 +0600

    account name uniqueness fix

--- backend/app/validationSchemas/accounting.js
+++ backend/app/validationSchemas/accounting.js
@@ -1,8 +1,23 @@
 import yup from "yup";
+import { Account } from "../models/index.js";
 
 export const create = yup.object({
   body: yup.object({
-    name: yup.string().required(),
+    name: yup
+      .string()
+      .test(
+        "checkName",
+        "Account with the same name already exists.",
+        function (v) {
+          const req = this.options.context.req;
+          const company = req.business?._id || req.authUser._id;
+          return Account.findOne({
+            company,
+            name: v,
+          }).then((data) => !data);
+        }
+      )
+      .required(),
     parent: yup.string().nullable(),
     type: yup
       .string()
@@ -24,7 +39,22 @@ export const create = yup.object({
 });
 export const update = yup.object({
   body: yup.object({
-    name: yup.string().required(),
+    name: yup
+      .string()
+      .test(
+        "checkName",
+        "Account with the same name already exists.",
+        function (v) {
+          const req = this.options.context.req;
+          const company = req.business?._id || req.authUser._id;
+          return Account.findOne({
+            _id: { $ne: req.params.id },
+            company,
+            name: v,
+          }).then((data) => !data);
+        }
+      )
+      .required(),
     parent: yup.string().nullable(),
     type: yup
       .string()
